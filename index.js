44e6f6da90f8121b6c88394b03e9c6002888e97a
Douglas <douglaskerins@gmail.com>
2021-11-05 11:50:20 +0000

    fix server

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, express!')
+let notes = [];
+
+app.get('/notes', (req, res) => {
+  res.send(JSON.stringify(notes));
+});
+
+app.post('/notes', (req, res) => {
+  const newNote = {
+    title: req.body.title,
+    content: req.body.content
+  };
+  notes.push(newNote);
+  res.send(JSON.stringify(newNote));
 });
 
 console.log(`Server listening on ${port}`);
 app.listen(port);
-app.use(cors());
